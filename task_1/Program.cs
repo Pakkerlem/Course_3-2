60b7a5787d481d723f2f8ed568c8e87dbfe402f8
Pakker <gfabinam@gmail.com>
2023-11-30 18:32:34 +0300

    first_task

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно
+// одновременно 7 и 23
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%7 == 0 && number %23 == 0)
+{
+    Console.WriteLine("Число:"+number +" кратно 7 и 23 " );
+}
+else
+{
+    Console.WriteLine("Число:"+number +" НЕ кратно 7 и 23 " );
+}
